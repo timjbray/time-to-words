8754355abb7dc28b6d3106e2da1c4de3c182d4bd
Tim Bray <tim@tebray.com>
2021-01-07 16:27:02 +1100

    initial commit

---
+++ time-to-words.js
@@ -0,0 +1,4 @@
+// expecting numeric time to be in a format like '8:15' or '12:30'
+export function convertTimeToWords(numericTime) {
+  return 'midnight';
+}
---
+++ time-to-words.test.js
@@ -0,0 +1,8 @@
+import { convertTimeToWords } from './time-to-words';
+
+describe('Time to words', () => {
+  it('Handles midnight', () => {
+    const timeInWords = convertTimeToWords('0:00');
+    expect(timeInWords).toBe('midnight');
+  });
+});
