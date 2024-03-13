0cc7f399dab9306d5ea74a7cdb1df0b4792cea9d
james <james.barbosa@gmail.com>
2024-03-13 12:05:18 +0800

    completed solution

--- index.js
+++ index.js
@@ -6,36 +6,35 @@ function convertTimeToWords(time) {
   } else if (time == '12:00') {
     return 'midday'
   } else {
-    return parseTime(time)
+    return convert(time)
   }
-
-  return 'half past eight';
 }
 
-const hourMap = {
+const numberToWordMap = {
   1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
   6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
-  11: 'eleven'
+  11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen'
+  , 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 21: 'twenty one',
+  22: 'twenty two', 23: 'twenty three', 24: 'twenty four', 25: 'twenty five', 26: 'twenty six'
+  ,27: 'twenty seven',28: 'twenty eight',29: 'twenty nine'
 }
 
-function parseTime(time) {
+function convert(time) {
   let [hour, minute] = time.split(":");
   let minuteNumber = +minute;
   let timeInterval = timeIntervals(minute);
   let conjunction = ''
   let newHour = +hour;
   if (minuteNumber > 0 && minuteNumber <= 30) {
-    conjunction = 'past'
-  } else {
-    conjunction = 'to'
-    newHour = newHour + 1;
+    newHour = numberToWordMap[newHour];
+    timeInterval = timeIntervals(minute)
+    return `${numberToWordMap[minuteNumber]} past ${newHour}`
+  } else if (minuteNumber > 30 && minuteNumber <= 60) {
+    newHour = numberToWordMap[newHour + 1];
+    return `${numberToWordMap[60 - minuteNumber]} to ${newHour}`
+  } else{
+    return `${numberToWordMap[newHour]} o'clock`
   }
-
-  if (timeInterval) {
-
-  }
-
-  return `${timeInterval} ${conjunction} ${newHour}`
 }
 
 function timeIntervals(minute) {
@@ -53,11 +52,21 @@ function timeIntervals(minute) {
       return 'five'
     }
     default: {
-      return ''
+      return numberToWordMap[minute]
     }
   }
 }
 
+console.log(convertTimeToWords('0:00'));
+console.log(convertTimeToWords('12:00'));
+console.log(convertTimeToWords('2:00'));
+console.log(convertTimeToWords('2:03'));
+console.log(convertTimeToWords('2:11'));
+console.log(convertTimeToWords('2:15'));
+console.log(convertTimeToWords('2:30'));
+console.log(convertTimeToWords('2:33'));
+console.log(convertTimeToWords('2:40'));
 console.log(convertTimeToWords('2:45'));
+console.log(convertTimeToWords('3:00'));
 
 module.exports = { convertTimeToWords };
