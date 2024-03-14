e1522304c11b3d1b46310546b15c6e1216d39e51
james <james.barbosa@gmail.com>
2024-03-14 09:59:59 +0800

    Added more unit tests

--- index.test.js
+++ index.test.js
@@ -7,6 +7,26 @@ describe('Time to words', () => {
     expect(timeInWords).toBe('midnight');
   });
 
+  it('Handles midday', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it('Handles 2:03', () => {
+    const timeInWords = convertTimeToWords('2:03');
+    expect(timeInWords).toBe('three past two');
+  });
+
+  it('Handles 2:11', () => {
+    const timeInWords = convertTimeToWords('2:11');
+    expect(timeInWords).toBe('eleven past two');
+  });
+
+  it('Handles 2:15', () => {
+    const timeInWords = convertTimeToWords('2:15');
+    expect(timeInWords).toBe('quarter past two');
+  });
+
   it('Handles 30 - 8:30', () => {
     const timeInWords = convertTimeToWords('8:30');
     expect(timeInWords).toBe('half past eight');
@@ -25,5 +45,10 @@ describe('Time to words', () => {
   it('Handles 9 oclock', () => {
     const timeInWords = convertTimeToWords('9:00');
     expect(timeInWords).toBe(`nine o'clock`);
+  });
+
+  it('Handles 5 minutes before 3 - 2:55', () => {
+    const timeInWords = convertTimeToWords('2:55');
+    expect(timeInWords).toBe(`five to three`);
   });
 });
