ebf595329f655104c76c9da6d7d21b09707e8971
james <james.barbosa@gmail.com>
2024-03-14 09:54:00 +0800

    Added unit tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const  { convertTimeToWords } = require('./index');
 
+
 describe('Time to words', () => {
   it('Handles midnight', () => {
     const timeInWords = convertTimeToWords('0:00');
@@ -14,5 +15,15 @@ describe('Time to words', () => {
   it('Handles times after 30 mins - 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
+  });
+
+  it('Handles times after 15 mins - 1:15', () => {
+    const timeInWords = convertTimeToWords('1:15');
+    expect(timeInWords).toBe('quarter past one');
+  });
+
+  it('Handles 9 oclock', () => {
+    const timeInWords = convertTimeToWords('9:00');
+    expect(timeInWords).toBe(`nine o'clock`);
   });
 });
