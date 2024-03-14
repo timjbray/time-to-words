536cecc5ec43ce0309bb18baa8bd462b358b2ca9
james <james.barbosa@gmail.com>
2024-03-14 10:08:56 +0800

    Extracted numberToWord map Refactored code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+import { numberToWordMap } from "./numberToWordMap";
+
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
   // TODO: real code goes here!
@@ -6,30 +9,17 @@ function convertTimeToWords(time) {
   } else if (time == '12:00') {
     return 'midday'
   } else {
-    return convert(time)
-  }
-}
-
-const numberToWordMap = {
-  1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-  6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
-  11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen'
-  , 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 21: 'twenty one',
-  22: 'twenty two', 23: 'twenty three', 24: 'twenty four', 25: 'twenty five', 26: 'twenty six'
-  , 27: 'twenty seven', 28: 'twenty eight', 29: 'twenty nine'
-}
-
-function convert(time) {
-  let [hour, minute] = time.split(":");
-  let minuteWord = minuteToWord(minute);
-  if (+minute > 0 && +minute <= 30) {
-    return `${minuteWord} past ${numberToWordMap[+hour]}`
-  } else if (+minute == 45) {
-    return `${minuteWord} to ${numberToWordMap[+hour + 1]}`
-  } else if (+minute > 30 && +minute < 60) {
-    return `${numberToWordMap[60 - +minute]} to ${numberToWordMap[+hour + 1]}`
-  } else {
-    return `${numberToWordMap[+hour]} o'clock`
+    let [hour, minute] = time.split(":");
+    let _minuteWord = minuteToWord(minute);
+    if (+minute > 0 && +minute <= 30) {
+      return `${_minuteWord} past ${numberToWordMap[+hour]}`
+    } else if (+minute == 45) {
+      return `${_minuteWord} to ${numberToWordMap[+hour + 1]}`
+    } else if (+minute > 30 && +minute < 60) {
+      return `${numberToWordMap[60 - +minute]} to ${numberToWordMap[+hour + 1]}`
+    } else {
+      return `${numberToWordMap[+hour]} o'clock`
+    }
   }
 }
 
@@ -52,17 +42,5 @@ function minuteToWord(minute) {
     }
   }
 }
-
-console.log(convertTimeToWords('0:00'));
-console.log(convertTimeToWords('12:00'));
-console.log(convertTimeToWords('2:00'));
-console.log(convertTimeToWords('2:03'));
-console.log(convertTimeToWords('2:11'));
-console.log(convertTimeToWords('2:15'));
-console.log(convertTimeToWords('2:30'));
-console.log(convertTimeToWords('2:33'));
-console.log(convertTimeToWords('2:40'));
-console.log(convertTimeToWords('2:45'));
-console.log(convertTimeToWords('2:55'));
 
 module.exports = { convertTimeToWords };
